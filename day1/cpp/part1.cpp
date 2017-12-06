087e1d2006356ec5cd56abcf91e84b5d2f153ee7
Ash Setter <ajs3g11@soton.ac.uk>
2017-12-06 18:58:11 +0000

    adding file reading

--- day1/cpp/part1.cpp
+++ day1/cpp/part1.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
+#define MAXFILELEN 1000
 
 int captcha_sum(int *sequence, int length){
   /*
@@ -22,16 +25,35 @@ int captcha_sum(int *sequence, int length){
 }
 
 int main(){
+  std::ifstream readfile;
   std::string input;
   int testarray[8] = {9, 1, 2, 1, 2, 1, 2, 9};
-  int length = 8;
+  int* numarray = NULL; // initialise pointer to int, initialise to NULL pointer
+  int length = 8; // get length of array
+  char filetext[MAXFILELEN];
+  
+  readfile.open("input.txt"); // open input file
+  if (readfile.is_open()){
+    while (!readfile.eof()){
+      readfile >> filetext;
+      std::cout << filetext << "\n";
+    }
+  }
+  readfile.close();
+  numarray = new int[length]; // allocate memory to store n ints
+  for (int i = 0; i < length; i++){
+    numarray[i] = testarray[i]; // initialise all elements of array
+  }
   int result = 0;
   
   input = "test";
   std::cout << input << "\n";
   
-  result = captcha_sum(testarray, length);
+  result = captcha_sum(numarray, length);
   std::cout << result << "\n";
+
+  delete [] numarray; // frees memory
+  numarray = NULL; // set numarray to NULL pointer again to avoid invalid memory access
   
   return 0;
 }
